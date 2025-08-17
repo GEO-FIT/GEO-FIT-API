5ac0dee06bf59ba2595e2ca0690070fb99871abf
krSeonghyeon <ya41921174@gmail.com>
2025-08-17 15:55:41 +0900

    init

---
+++ src/main/java/com/geofit/geofit/GeofitApplication.java
@@ -0,0 +1,13 @@
+package com.geofit.geofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeofitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeofitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geofit/geofit/GeofitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geofit.geofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeofitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
